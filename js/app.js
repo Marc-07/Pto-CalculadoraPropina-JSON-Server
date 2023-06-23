e32dc58412e1aab4308c7bbffa460040ec10e2de
Marc-07 <maanraca01@gmail.com>
2023-06-23 16:30:17 -0500

    Se agrega los input de cantidad

--- js/app.js
+++ js/app.js
@@ -89,11 +89,22 @@ function mostrarPlatillos(platillos){
         const categoria = document.createElement('DIV');
         categoria.classList.add('col-md-3');
         categoria.textContent = categorias [platillo.categoria];
-        
+
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'Number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `Producto-${platillo.id}`;
+        inputCantidad.classList.add('form-control')
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
 
         row.appendChild(nombre);
         row.appendChild(precio);
         row.appendChild(categoria);
+        row.appendChild(agregar)
 
         contenido.appendChild(row);
     })
